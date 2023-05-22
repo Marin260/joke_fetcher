93c1559bf230d730a28018df33489b872b99696e
Marin Martuslovic <marin.martuslovic@own.solutions>
2023-05-22 22:59:32 +0200

    Initial unit tests for joke fetching API endpoint

--- src/chuck-norris-api/test/chuck-norris-controller.test.ts
+++ src/chuck-norris-api/test/chuck-norris-controller.test.ts
@@ -1,21 +1,39 @@
 import { Request, Response } from 'express';
 
 import { ChuckNorrisController } from '../routes';
-describe('ChuckNorrisController tests', () => {
-  test('GET /health-check/', async () => {
-    const chuckApi = new ChuckNorrisController();
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest
-        .fn()
-        .mockReturnValue({ message: 'Everything up and running...' }),
-    };
 
+describe('ChuckNorrisController tests', () => {
+  const chuckApi = new ChuckNorrisController();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const mockRequest = (req?: { [key: string]: any }) => req;
+
+  const mockResponse = (msg: string) =>
+    ({
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnValue({ message: msg }),
+    } as unknown as Response);
+
+  test('GET /chuck-norris/health-check/', async () => {
     const response = await chuckApi.test(
-      {} as Request,
-      mockResponse as unknown as Response
+      mockRequest({}) as Request,
+      mockResponse('Everything up and running...')
     );
 
     expect(response).toEqual({ message: 'Everything up and running...' });
+  });
+
+  // TODO: mock database fail in health check...
+  test('GET /chuck-norris/', async () => {
+    const response = await chuckApi.get(
+      mockRequest({
+        body: { authenticatedUser: 'john.doe@acme.com' },
+      }) as Request,
+      mockResponse(
+        'A new joke has been sent to your email'
+      ) as unknown as Response
+    );
+    expect(response).toEqual({
+      message: 'A new joke has been sent to your email',
+    });
   });
 });
