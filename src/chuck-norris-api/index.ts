bded1a8ae84154e745b1e24bb48066d884b75cc9
Marin260 <marin.martuslovic@gmail.com>
2023-05-28 22:04:33 +0200

    removed deprecated body-parser package

--- src/chuck-norris-api/index.ts
+++ src/chuck-norris-api/index.ts
@@ -1,13 +1,12 @@
 import { Server } from '@overnightjs/core';
-import * as bodyParser from 'body-parser';
+import { json } from 'express';
 import * as routes from './routes';
 import { db } from './../database-entities';
 
 export class JokeFetcherApp extends Server {
   constructor() {
     super();
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(json());
     this.addControllers(Object.values(routes).map((Route) => new Route()));
   }
 
